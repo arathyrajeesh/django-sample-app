67063b3f4c524529f2d44ba68b7eed8e379ff53c
Arathy <arathyrajeesh2@gmail.com>
2025-09-24 10:56:07 +0530

    perform add,delete, update

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.main,name='main'),
-    path('home/',views.index ),
+    path('home/',views.index ,name='index'),
     path('home/details/<int:id>', views.details, name='details'),
-    path('testing',views.testing)
+    path('testing',views.testing),
+    path('add/', views.add, name='add'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from app.models import member
 
@@ -21,3 +21,34 @@ def testing(request):
         'fruits': ['Apple', 'Banana', 'Cherry'],   
     }
     return render(request,'testing.html',context)
+
+
+def add(request):
+    return render(request,'add.html')
+
+
+def add_record(request):
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    new_member = member(firstname=fname, lastname=lname)  
+    new_member.save()
+    return redirect('index')  
+
+
+def delete(request, id):
+    person = member.objects.get(id=id)
+    person.delete()
+    return redirect('index')
+
+def update(request, id):
+    person = member.objects.get(id=id)
+    
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        person.firstname = fname
+        person.lastname = lname
+        person.save()
+        return redirect('index')
+    
+    return render(request, 'update.html', {'person': person})
